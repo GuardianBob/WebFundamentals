3a46688fdac83311ab8489796ded0eaaf6ea0a7a
GuardianBob <78508863+GuardianBob@users.noreply.github.com>
2021-02-18 14:49:26 -0800

    JavaScript practice

---
+++ JavaScript/arrayPredict.js
@@ -0,0 +1,48 @@
+function pred1() {
+    var arr = [8,6,7,5,3,0,9]
+    for(var i = 0; i < arr.length; i++){    
+        console.log(arr[i]);
+    }
+}
+//Output Prediction: 8, 6, 7, 5, 3, 0, 9
+console.log("1st Array Output:");
+pred1();
+
+function pred2() {
+    var arr = [7,3,8,4,2,0,1];
+    for(var i = 0; i < arr.length; i++){ 
+        if(arr[i] % 2 == 0){
+            console.log(arr[i]);
+        } 
+        else{
+            console.log("That is odd!");
+        }
+    }
+}
+//Output Prediction: That is odd!, That is odd!, 8, 4, 2, 0, That is odd!
+console.log("2nd Array Output:");
+pred2();
+
+function pred3() {
+    var arr = [1,3,8,-5,0,-2,4,-1];
+    var newArr = [];
+    for(var i = 0; i< arr.length; i++){
+        if(arr[i] < 0){
+            newArr.push(arr[i]);            // 
+            arr[i] = arr[i] * -1;           //
+        }
+        else if(arr[i] == 0){               
+            arr[i] = "Zero";
+        }
+        else{
+            arr[i] = arr[i] * -1;           
+        }
+    }
+    console.log(arr);
+    console.log(newArr);
+}
+//Output Prediction: 
+//          arr:        -1, -3, -8, 5, Zero, 2, -4, 1
+//          newArr:     -5, -2, -1
+console.log("3rd Array Output:");
+pred3();
